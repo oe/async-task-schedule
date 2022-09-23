958407f2925afe9e9b9eb0a5d76415629eb69b55
Saiya <oklark@outlook.com>
2022-09-23 09:47:38 +0800

    chore: add test case

--- test/async-task.spec.ts
+++ test/async-task.spec.ts
@@ -19,5 +19,20 @@ describe('async-task-schedule', () => {
       // @ts-ignore
       expect(result4).toEqual(result3[2][1])
     })
+
+    it("should get a not found error when result is missing", async () => {
+      const at = new AsyncTask({
+        // @ts-ignore
+        batchDoTasks: async (names: number[]) => {
+          return names.map((n) => n % 2 ? ([n, `${n}-result`] as [number, string]) : false).filter(Boolean)
+        },
+      })
+
+      const result = await at.dispatch([1, 2, 3])
+      const respFor2 = result.find(item => item[0] === 2)
+      expect(respFor2).toBeDefined()
+      expect(respFor2![1]).toBeInstanceOf(Error)
+      expect(result[0][1]).toContain('-result')
+    })
   })
 })
